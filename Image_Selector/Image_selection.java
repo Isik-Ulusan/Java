ccb861f726ebe1988d35d1dadd26dda746efea4e
Isik-Ulusan <107641999+Isik-Ulusan@users.noreply.github.com>
2022-07-27 13:45:31 +0300

    Create Image_selection.java

---
+++ Image_Selector/Image_selection.java
@@ -0,0 +1,73 @@
+package java_form;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JComboBox;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.ImageIcon;
+import javax.swing.SwingConstants;
+
+public class Image_selection extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Image_selection frame = new Image_selection();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Image_selection() {
+		setResizable(false);
+		setTitle("Image Selection");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 464, 535);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Index:");
+		lblNewLabel.setBounds(10, 11, 46, 14);
+		contentPane.add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("");
+		lblNewLabel_1.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel_1.setBounds(10, 63, 428, 422);
+		contentPane.add(lblNewLabel_1);
+		
+		String[] arr = {"1", "2", "3", "4", "5", "6"};
+		JComboBox comboBox = new JComboBox(arr);
+		comboBox.setBounds(10, 30, 164, 22);
+		contentPane.add(comboBox);
+		
+		JButton btnNewButton = new JButton("Select Image");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				lblNewLabel_1.setIcon(new ImageIcon(Image_selection.class.getResource("/img/character"+comboBox.getSelectedItem()+".png")));
+			}
+		});
+		btnNewButton.setBounds(261, 30, 114, 23);
+		contentPane.add(btnNewButton);
+		
+	}
+}
